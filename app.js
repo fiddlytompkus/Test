c94d1c4b18f571d72cbbb0106931fc73071bf651
Yash1256 <shuklayash1256@gmail.com>
2021-01-22 18:19:02 +0530

    Added MongoDatabase 😎

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database Successfully Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
